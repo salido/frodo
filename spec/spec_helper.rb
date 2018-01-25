74a6d995ed82616bfc5b5e2e27149b26e895ec93
Ari Perez <aris.a.perez@gmail.com>
2018-01-24 20:53:32 -0500

    Add pundit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,14 @@ require 'bundler/setup'
 Bundler.setup
 
 require 'bson'
-require 'pry-byebug'
-require 'grape'
 require 'frodo'
-require 'support/shared_context'
+require 'grape'
 require 'json'
 require 'ostruct'
+require 'pry-byebug'
+require 'pundit'
+require 'support/shared_context'
+require 'support/frodo_user'
 require 'webmock/rspec'
 
 RSpec.configure(&:raise_errors_for_deprecations!)
