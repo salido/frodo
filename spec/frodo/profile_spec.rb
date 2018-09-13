e17f446ab9cb337ba35b188c373c86f4a32a0cae
David Chan <chand@chandeeland.org>
2018-09-13 15:31:52 -0400

    put the attribute[data] layer back

--- spec/frodo/profile_spec.rb
+++ spec/frodo/profile_spec.rb
@@ -49,32 +49,38 @@ describe Frodo::Profile do
       let(:status) { 200 }
 
       let(:body) do
-        JSON.unparse(data: {
-                       id: 'dfb70c18-0677-4e4f-bce4-17158eba0ecf',
-                       type: 'profiles',
-                       attributes: {
-                         version: 1,
-                         'configs' =>
-                             { 'LEVELUP_TEST' => { 'value' => '2', 'value_type' => nil },
-                               'SALIDO_POS_TEST' => { 'value' => '1', 'value_type' => 'int' },
-                               'LEVELUP_API_TOKEN' => { 'value' =>
-                                 'db6c952e67260514f68d43e3b038ceeaa10d54f23c8d3bf106d742fd2b489d8cMerchant',
-                                                        'value_type' => 'Brand' },
-                               'LEVELUP_MERCHANT_ID' => { 'value' => '194', 'value_type' => 'Brand' },
-                               'SALIDO_POS_SOME CONFIG NAME' =>
-                                 { 'value' => 'some config value', 'value_type' => 'String' } },
-                         'client_applications' => { '11' => 'SALIDO_POS', '12' => 'LEVELUP' }
-                       },
-                       expires_at: nil,
-                       scopable_type: 'Group',
-                       scopable_id: 'c1d64eca-d1ff-4569-9b7d-f049866160f1',
-                       created_at: 'Wed, 29 Aug 2018 14:52:39 UTC +00:00',
-                       updated_at: 'Wed, 29 Aug 2018 14:52:39 UTC +00:00'
-                     })
+        JSON.unparse(
+          data: {
+            id: 'dfb70c18-0677-4e4f-bce4-17158eba0ecf',
+            type: 'profiles',
+            attributes: {
+              version: 1,
+              data: {
+                'configs' => {
+                  'LEVELUP_TEST' => { 'value' => '2', 'value_type' => nil },
+                  'SALIDO_POS_TEST' => { 'value' => '1', 'value_type' => 'int' },
+                  'LEVELUP_API_TOKEN' => { 'value' =>
+                  'db6c952e67260514f68d43e3b038ceeaa10d54f23c8d3bf106d742fd2b489d8cMerchant',
+                                           'value_type' => 'Brand' },
+                  'LEVELUP_MERCHANT_ID' => { 'value' => '194', 'value_type' => 'Brand' },
+                  'SALIDO_POS_SOME CONFIG NAME' =>
+                  { 'value' => 'some config value', 'value_type' => 'String' }
+                },
+                'client_applications' => { '11' => 'SALIDO_POS', '12' => 'LEVELUP' }
+              }
+            },
+            expires_at: nil,
+            scopable_type: 'Group',
+            scopable_id: 'c1d64eca-d1ff-4569-9b7d-f049866160f1',
+            created_at: 'Wed, 29 Aug 2018 14:52:39 UTC +00:00',
+            updated_at: 'Wed, 29 Aug 2018 14:52:39 UTC +00:00'
+          }
+        )
       end
 
       it 'returns the gandalf profile' do
-        expect(subject['data']['attributes']['configs']['SALIDO_POS_TEST']).to eq('value' => '1', 'value_type' => 'int')
+        expect(subject['data']['attributes']['data']['configs']['SALIDO_POS_TEST'])
+          .to eq('value' => '1', 'value_type' => 'int')
       end
     end
   end
@@ -115,32 +121,38 @@ describe Frodo::Profile do
       let(:status) { 200 }
 
       let(:body) do
-        JSON.unparse(data: {
-                       id: 'dfb70c18-0677-4e4f-bce4-17158eba0ecf',
-                       type: 'profiles',
-                       attributes: {
-                         version: 1,
-                         'configs' =>
-                             { 'LEVELUP_TEST' => { 'value' => '2', 'value_type' => nil },
-                               'SALIDO_POS_TEST' => { 'value' => '1', 'value_type' => 'int' },
-                               'LEVELUP_API_TOKEN' => { 'value' =>
-                                 'db6c952e67260514f68d43e3b038ceeaa10d54f23c8d3bf106d742fd2b489d8cMerchant',
-                                                        'value_type' => 'Brand' },
-                               'LEVELUP_MERCHANT_ID' => { 'value' => '194', 'value_type' => 'Brand' },
-                               'SALIDO_POS_SOME CONFIG NAME' =>
-                                 { 'value' => 'some config value', 'value_type' => 'String' } },
-                         'client_applications' => { '11' => 'SALIDO_POS', '12' => 'LEVELUP' }
-                       },
-                       expires_at: nil,
-                       scopable_type: 'Group',
-                       scopable_id: 'c1d64eca-d1ff-4569-9b7d-f049866160f1',
-                       created_at: 'Wed, 29 Aug 2018 14:52:39 UTC +00:00',
-                       updated_at: 'Wed, 29 Aug 2018 14:52:39 UTC +00:00'
-                     })
+        JSON.unparse(
+          data: {
+            id: 'dfb70c18-0677-4e4f-bce4-17158eba0ecf',
+            type: 'profiles',
+            attributes: {
+              version: 1,
+              data: {
+                'configs' => {
+                  'LEVELUP_TEST' => { 'value' => '2', 'value_type' => nil },
+                  'SALIDO_POS_TEST' => { 'value' => '1', 'value_type' => 'int' },
+                  'LEVELUP_API_TOKEN' => { 'value' =>
+                   'db6c952e67260514f68d43e3b038ceeaa10d54f23c8d3bf106d742fd2b489d8cMerchant',
+                                           'value_type' => 'Brand' },
+                  'LEVELUP_MERCHANT_ID' => { 'value' => '194', 'value_type' => 'Brand' },
+                  'SALIDO_POS_SOME CONFIG NAME' =>
+                   { 'value' => 'some config value', 'value_type' => 'String' }
+                },
+                'client_applications' => { '11' => 'SALIDO_POS', '12' => 'LEVELUP' }
+              }
+            },
+            expires_at: nil,
+            scopable_type: 'Group',
+            scopable_id: 'c1d64eca-d1ff-4569-9b7d-f049866160f1',
+            created_at: 'Wed, 29 Aug 2018 14:52:39 UTC +00:00',
+            updated_at: 'Wed, 29 Aug 2018 14:52:39 UTC +00:00'
+          }
+        )
       end
 
       it 'returns the gandalf profile' do
-        expect(subject['data']['attributes']['configs']['SALIDO_POS_TEST']).to eq('value' => '1', 'value_type' => 'int')
+        expect(subject['data']['attributes']['data']['configs']['SALIDO_POS_TEST'])
+          .to eq('value' => '1', 'value_type' => 'int')
       end
     end
   end
